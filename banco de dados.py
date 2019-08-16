5628ca1d95f0d6eb50058fda13fa63880e9e62c7
danilowOENS <dl539994@gmail.com>
2019-08-16 17:38:26 -0300

    fazendo dados de um banco

---
+++ banco de dados.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+print("bem-vindo(a) ao GROGER BANK, para continuar cadastre as seguintes informaçoes:")
+
+nome = input("digite seu nome inteiro:")
+anos = int(input("digite quantos anos tem:"))
+saldo = float(input("digite quanto de saldo tem:"))
+
+def saldo1 (saldo):
+    valor = float(input("digite valor do saque: ")
+        if valor <= 1000:
+            print("saque permitido")
+        saldo = saldo-valor
+    print(saldo1)
+
+def atm(nome, anos, saldo):
+    digite = int(input("""(1) para saque\n
+                          (2) para deposito\n
+                          (3) para empréstimo\n
+                          (4) para visualizar informaçoes"""))
+
+if digite == 1:
+    print ("digite (1) para saque ")
+
+elif digite == 2:
+    print ("digite (2) para depósito")
+
+elif digite == 3:
+    print ("digite (3) para empréstimo")
+
+elif digite == 4:
+    print ("digite (4) para visualizar informaçoes")
+
+
+
